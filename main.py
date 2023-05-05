29b8ab9eef042ea3c6c6f709995c73017e4072c2
ezratweaver <101545981+ezratweaver@users.noreply.github.com>
2023-05-04 19:13:52 -0500

    Confirm Spoofing Added

--- main.py
+++ main.py
@@ -10,21 +10,28 @@ lesson_data = grab_lesson_data()
 progress, progress_percentage, grade_percentage, grade = sift_lesson_data(
     lesson_data)
 
-system('cls')
-print('Welcome to Teaching Textbooks Algebra 1 Lesson Spoofer\n')
+system("cls")
+print("Welcome to Teaching Textbooks Algebra 1 Lesson Spoofer\n")
 print(f"Current Progress: {progress} ({progress_percentage})")
 print(f"Current Grade: {grade_percentage} ({grade})\n")
 
-user_input_lesson_start = int(input("Type starting lesson >>> "))
-user_input_lesson_end = int(input("Type ending lesson >>> "))
+user_input_confirm_spoof = input("Do you want to spoof? (Y/N) ")
 
-system('cls')
+if user_input_confirm_spoof.upper() == "Y":
+    system("cls")
+    user_input_lesson_start = int(input("Type starting lesson >>> "))
+    user_input_lesson_end = int(input("Type ending lesson >>> "))
+else:
+    system("cls")
+    quit()
+
+system("cls")
 for x in range(user_input_lesson_start, user_input_lesson_end + 1):
     print(f"Spoofing Lesson {x}...")
     sleep(1)
     spoof_lesson(x, ADD_WRONG)
 
-system('cls')
-print(f'Done! Spoofed lessons {user_input_lesson_start}-{user_input_lesson_end}')
+system("cls")
+print(f"Done! Spoofed lessons {user_input_lesson_start}-{user_input_lesson_end}")
 
 db_connection.close()
